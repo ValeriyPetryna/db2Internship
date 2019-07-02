0e880e36bd92670d6c6f3e9a0077e207c860fa2f
valerii.petryna <valerii.petryna@hive-studio.net>
2019-07-02 14:57:14 +0300

    improve pug

--- homework1/gulpfile.js
+++ homework1/gulpfile.js
@@ -1,12 +1,56 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-  return gulp.src('./app/**/*.sass')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./app'))
-})
+function style() {
+  return gulp
+    .src('./src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+}
 
-gulp.task('sass:watch', function(){
-    gulp.watch('./app/**/*.scss', ['sass'])
-  })
+function template() {
+  return gulp
+    .src('./src/*.pug')
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+}
+
+function imagesRendering() {
+  return gulp
+    .src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+}
+
+async function build() {
+  await style();
+  await template();
+  await imagesRendering();
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+      index: 'index.html'
+    }
+  });
+  gulp.watch('./src/sass/**/*.scss', style);
+  gulp.watch('./src/*.pug', template);
+}
+
+exports.style = style;
+exports.template = template;
+exports.imagesRendering = imagesRendering;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
